22b0071d7c4d0442c7dc61b99b26b04f8681fd8e
reneczka <46569696+reneczka@users.noreply.github.com>
2022-08-02 16:45:17 +0200

    1.2 dodawanie modelu przepisu (#2)
    * 1.2 dodawanie modelu przepisu
    
    * poprawione

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=225)
+    ingredients = models.TextField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    preparation_time = models.IntegerField()
+    votes = models.IntegerField(default=0)
+
