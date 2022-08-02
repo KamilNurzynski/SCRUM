1d1fcebeb05560801034c144de97dcc018b8d0ea
Bartosz Kozikowski <106819506+bartkozik@users.noreply.github.com>
2022-08-02 15:13:50 +0200

    dodanie urls (#1)
    * dodanie urls
    
    * dodany widok karuzeli i template do podpięcia na stronie
    
    * dodany widok karuzeli i htlm do podpięcia
    
    * dodany widok w URL's
    
    * zmiany w widokach

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 
 from django.shortcuts import render
@@ -9,3 +10,15 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+
+class CarouselView(View):
+
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        random_recipes = []
+        for random_count in range(3):
+            recipe = random.choice(recipes)
+            random_recipes.append(recipe)
+        return render(request, 'carousel.html', {'recipes': random_recipes})
